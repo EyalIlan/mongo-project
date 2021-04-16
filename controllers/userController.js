96ddc56bd5ab60766fefa7f3b30b69c57250a7b9
Your Name <you@example.com>
2021-04-16 11:43:00 +0300

    add deposit method

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,11 @@
-
 const fs = require('fs')
-
-let id = 0
+const generateUniqueId = require('generate-unique-id');
+ 
 
 const addUser = (req,res) =>{
     
-    id++;
+    const id = generateUniqueId();
+
     let name = req.body.name
     
     fs.readFile('users.json',(error,data)=>{
@@ -16,11 +16,42 @@ const addUser = (req,res) =>{
 
         fs.writeFile('users.json',JSON.stringify(data),err => console.log(err))
             
+ })
 
+}
+
+const Depositing = (req,res) =>{
+
+    let id = req.params.id
+    let deposit = req.body.deposit
+
+
+    fs.readFile('users.json',(err,data)=>{
+        data = JSON.parse(data)
+        let index = data.map(p =>{
+            return p.id
+        }).indexOf(id)
+        
+        if(index && deposit>0){
+            
+            // console.log(index)
+            data[index].cash += deposit
+            console.log(data)
+            
+            fs.writeFile('users.json',JSON.stringify(data),(err) =>{
+                console.log(err)
+            })
+            res.write(`${data[index].name} got ${data[index].cash + deposit} in his account`)
+        }else{
+            res.write('User id does not exist in the system or the deposit was negative')
+        }
+        res.end()
     })
 
 }
 
+
 module.exports = {
-    addUser
+    addUser,
+    Depositing
 }
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -5,7 +5,9 @@ const  userController = require('../controllers/userController')
 const router = express.Router()
 
 
+
 router.post('/add',userController.addUser)
 
+router.post('/deposit/:id',userController.Depositing)
 
 module.exports = router
